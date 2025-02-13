dd9d7a9826cc693f043d371845f474c276d6c7d2
Matheus Hygino <matheusghygino@gmail.com>
2025-02-13 08:40:44 -0300

    estilizada a busca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
 import Header from "../../components/Header";
-
+import background from "../../assets/background2.png"
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app"/> 
+        <div className="info">
+          <input name="usuario" placeholder="@username" />
+          <button>Buscas</button>
+        </div>
+        <div>
+          <img src="" className="profile" />
+        </div>
+      </div>
     </div>
   );
 }
