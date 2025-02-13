1effb6dc9d5244a53a6084b3e4987d5b733f1794
Matheus Hygino <matheusghygino@gmail.com>
2025-02-13 09:00:26 -0300

    criado o cabeçalho com informações do perfil

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,27 @@
 import Header from "../../components/Header";
-import background from "../../assets/background2.png"
-import './styles.css'
+import background from "../../assets/background2.png";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="conteudo">
-        <img src={background} className="background" alt="background app"/> 
+        <img src={background} className="background" alt="background app" />
         <div className="info">
-          <input name="usuario" placeholder="@username" />
-          <button>Buscas</button>
-        </div>
-        <div>
-          <img src="" className="profile" />
+          <div>
+            <input name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+          </div>
+          <div className="perfil">
+            <img src="" className="profile" alt="foto de perfil" />
+            <div>
+              <h3>Matheus Hygino</h3>
+              <span>@matheusghygino</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
