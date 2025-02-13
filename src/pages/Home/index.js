a5816839d112053394c80e9a4c633e7da558cf42
Matheus Hygino <matheusghygino@gmail.com>
2025-02-13 19:09:09 -0300

    criado e estilizado o ItemList component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import Header from "../../components/Header";
 import background from "../../assets/background2.png";
 import "./styles.css";
+import ItemList from "../../components/ItemList";
 
 function App() {
   return (
@@ -22,6 +23,21 @@ function App() {
             </div>
           </div>
           <hr />
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList 
+              title="Teste1"
+              description="teste de descrição"
+            />
+            <ItemList 
+              title="Teste1"
+              description="teste de descrição"
+            />
+            <ItemList 
+              title="Teste1"
+              description="teste de descrição"
+            />
+          </div>
         </div>
       </div>
     </div>
